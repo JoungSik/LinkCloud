da8a159af81fee1a33a796f0b6de686ea2e0e07e
JoungSik <tjstlr2010@gmail.com>
2021-11-13 01:50:38 +0900

    #5 유저 모델 + login, logout, register api 구현 완료

---
+++ app/controllers/v1/users/sessions_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class V1::Users::SessionsController < Devise::SessionsController
+  wrap_parameters :v1_user, format: [:url_encoded_form, :multipart_form, :json]
+  respond_to :json
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render 'v1/users/sessions/create', status: :ok
+  end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_user
+
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: "로그아웃 되었습니다." }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: "로그아웃에 실패했습니다." }, status: :unauthorized
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
   get "*page", to: "home#index", constraints: -> (req) do
     !req.xhr? && req.format.html?
   end
+
+  namespace :v1 do
+    devise_for :users, path: "",
+               path_names: { sign_in: :login, sign_out: :logout, registration: :users },
+               controllers: {
+                 sessions: 'v1/users/sessions',
+                 registrations: 'v1/users/registrations'
+               }
+  end
 end
