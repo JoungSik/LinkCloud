9d00bc91f8b0e92f62f27e0ba6530c9782a98ae3
JoungSik <tjstlr2010@gmail.com>
2021-11-13 22:31:32 +0900

    #5 클라이언트 코드 이동 완료

--- app/controllers/v1/users/sessions_controller.rb
+++ app/controllers/v1/users/sessions_controller.rb
@@ -34,9 +34,7 @@ class V1::Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    log_out_success && return if current_user
-
-    log_out_failure
+    log_out_success
   end
 
   def log_out_success
---
+++ app/javascript/client/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { ChakraProvider } from '@chakra-ui/react'
+import Routers from './routes';
+import theme from './utils/theme';
+import useLocalStorage from './utils/storage';
+
+const queryClient = new QueryClient();
+
+const App = () => {
+    const { storedValue } = useLocalStorage('user');
+    const [isLoggedIn, setIsLoggedIn] = useState(() => !!storedValue);
+
+    useEffect(() => {
+        setIsLoggedIn(!!storedValue);
+    }, [storedValue]);
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider theme={theme}>
+                <Routers isLoggedIn={isLoggedIn} />
+            </ChakraProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    )
+};
+
+export default App;
