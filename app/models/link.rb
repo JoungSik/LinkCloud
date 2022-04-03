74977c6eafff41a645d054fe17fa910105ad7133
JoungSik <tjstlr2010@gmail.com>
2022-04-03 17:09:32 +0900

    링크에 아무 값도 안넣어도 저장이 되는 이슈 수정 (#23)

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :user
 
+  validates_presence_of :name, :url, :user
   validates_uniqueness_of :name, scope: :user_id
   validates_uniqueness_of :url, scope: :user_id
 end
