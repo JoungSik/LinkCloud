767918059687a0a3383b678ed4eea61176b59792
JoungSik <tjstlr2010@gmail.com>
2022-04-03 16:06:07 +0900

    내가 저장한 링크 목록을 볼 수 있는 페이지

---
+++ app/views/v1/links/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @links, partial: "v1/links/v1_link", as: :link
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  root 'home#index'
+  root "home#index"
 
   devise_for :users, path: "",
              path_names: { sign_in: :login, sign_out: :logout, registration: :users },
              controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations',
-               passwords: 'users/passwords'
+               sessions: "users/sessions",
+               registrations: "users/registrations",
+               passwords: "users/passwords",
              }
+
+  scope module: :v1 do
+    resources :links
+  end
 end
